52b1cf8371f573ff770dfafeb7cdd4985e7756a7
muramatsu <seiya.etoo@gmail.com>
2014-03-29 21:33:41 +0900

    設定ファイルとサーブレットを作成

---
+++ src/main/java/muras/puzzclo/server/servlet/WebSocketServletImpl.java
@@ -0,0 +1,16 @@
+package muras.puzzclo.server.servlet;
+
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+public class WebSocketServletImpl extends WebSocketServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void configure(WebSocketServletFactory factory) {
+		//Listenerクラスを登録
+		factory.register(muras.puzzclo.server.servlet.PuzzcloWebSocketListener.class);
+	}
+
+}
